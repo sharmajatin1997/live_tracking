0d0d8d30a471d933c73fe2a77aa51b14c9ae7445
Jatin Sharma <80152469+sharmajatin1997@users.noreply.github.com>
2025-12-10 15:55:13 +0530

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
-const String google_api_key = "AIzaSyAVEUElTCFnEs87jTZ70aQN4pKycjroKf4";
+const String google_api_key = "MAP_KEY";
 const Color primaryColor = Color(0xFF7B61FF);
 const double defaultPadding = 16.0;
